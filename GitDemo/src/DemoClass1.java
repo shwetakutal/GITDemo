50d92fbffe2956fbbda59f9878decce0e867fe9f
shwetakutal <sks199430@gmail.com>
2023-03-31 00:27:48 +0530

    conflict with branch shweta2

--- GitDemo/src/DemoClass1.java
+++ GitDemo/src/DemoClass1.java
@@ -6,8 +6,8 @@ public class DemoClass1 {
 		System.out.println("Hello Shweta K. Shinde");
 		System.out.println("Creating conflict");
 		System.out.println("Creating conflict 2");
-	
 		System.out.println("Good Morning");
+		System.out.println("conflict with branch shweta2");
 		
 
 	}
