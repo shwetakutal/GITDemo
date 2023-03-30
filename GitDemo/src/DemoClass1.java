3d6b0b056179ef9f75672d76961d6658b5bbeb22
shwetakutal <sks199430@gmail.com>
2023-03-30 23:49:40 +0530

    changes in line no. 5

--- GitDemo/src/DemoClass1.java
+++ GitDemo/src/DemoClass1.java
@@ -2,7 +2,7 @@
 public class DemoClass1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Shweta");
+		System.out.println("Hello Shweta K. Shinde");
 		System.out.println("Good Morning");
 		
 
