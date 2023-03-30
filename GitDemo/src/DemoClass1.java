6bed89d4ca7703afd5804099cf5b5b0a612965fe
shwetakutal <sks199430@gmail.com>
2023-03-30 23:58:08 +0530

    conflict

--- GitDemo/src/DemoClass1.java
+++ GitDemo/src/DemoClass1.java
@@ -3,6 +3,7 @@ public class DemoClass1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Shweta K. Shinde");
+		System.out.println("Creating conflict");
 		System.out.println("Good Morning");
 		
 
