3139d856c0414ae7d9653165347defcaa20395c8
shwetakutal <sks199430@gmail.com>
2023-03-31 00:26:51 +0530

    changes in line 10

--- GitDemo/src/DemoClass1.java
+++ GitDemo/src/DemoClass1.java
@@ -7,7 +7,7 @@ public class DemoClass1 {
 		System.out.println("Creating conflict");
 		System.out.println("Creating conflict 2");
 		System.out.println("Good Morning");
-		System.out.println("Started shweta2 branch");
+		System.out.println("Started working on shweta2 branch");
 	
 		
 
